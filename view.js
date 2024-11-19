f63eb4da6fd702173bf7213fe06db9082320dfc9
rayong <skdyds@gmail.com>
2024-11-19 10:39:56 +0900

    제이쿼리 자바스크립트로 변경

--- view.js
+++ view.js
@@ -1,15 +1,21 @@
-// const liAr = [...document.querySelectorAll(".text")];
+// 자바스크립트
 
-// const viewText = (e) => {
-//   console.log(e.target);
-// };
+const liAr = [...document.querySelectorAll(".text")];
+const text = document.querySelector(".text");
 
-// liAr.map((li) => {
-//   li.addEventListener("click", viewText);
-// });
-
-$(".text").click(function (e) {
-  const t = $(e.target).text();
-  $(".text_wrap p").text(t);
+const viewText = (e) => {
+  text.innerText = e.target.innerText;
   gsap.from(".text_wrap", { opacity: 0, y: 30, duration: 0.5 });
+};
+
+liAr.map((li) => {
+  li.addEventListener("click", viewText);
 });
+
+// 제이쿼리
+
+// $(".text").click(function (e) {
+//   const t = $(e.target).text();
+//   $(".text_wrap p").text(t);
+//   gsap.from(".text_wrap", { opacity: 0, y: 30, duration: 0.5 });
+// });
